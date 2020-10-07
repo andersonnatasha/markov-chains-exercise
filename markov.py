8426f340168c7093493f2bf899118e61ff41d040
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-07 20:04:05 +0000

    completed project

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
+from sys import argv
+
 """Generate Markov text from text files."""
 
-from random import choice
+from random import choice 
 
 
 def open_and_read_file(file_path):
@@ -40,29 +42,51 @@ def make_chains(text_string):
         [None]
     """
     words = text_string.split()
-
     chains = {}
-
     for i in range(len(words)-2):
-        bigrams = (words[i], words[i+1])
-        word_following_bigrams = (words[i+2])
-        chains[bigrams] = word_following_bigrams
-        
+        bigram = (words[i], words[i+1])
+        word_following_bigram = words[i+2]
+        existing_words = chains.get(bigram, []) #creates key with blank list; existing_words is the value
+        existing_words.append(word_following_bigram) #appends following word to the empty list
+        chains[bigram] = existing_words    #stores new appended list back into the dictionary
+       
     return chains
 
 
 
 def make_text(chains):
     """Return text from chains."""
-
+    
     words = []
+    #random_value = choice(chains) #randomly chooses valu from the dictionary
+    # random_key = random_value.values() 
+    random_key = choice(list(chains))
+    words.extend(random_key)
+    while True:
+        random_value = choice(chains[random_key])
+        words.append(random_value)
+        random_key = (random_key[1], random_value)
+        if random_key not in chains:
+            break
+    
+    #print(random_value)
+    #loops
+    # while True:
+    #     if link not in chains:
+    #         break
+    #     else:
+    #         random_val = random.choice(chains[chains[key]])
+        #start with link = random key and then randomly select value from that key
+        #put link into list
+        #second link = second key value from the previous and the random value, and the value for that one is selected randomly
+        #when key is not in dictionary break 
+    #Get link out of list form and convert to a string
 
-    # your code goes here
 
     return ' '.join(words)
 
 
-input_path = 'green-eggs.txt'
+input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
